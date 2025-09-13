5d7a65d25768438cfd54118be39412610260a211
Vincent Nguyen <phucvn166@gmail.com>
2025-09-13 17:38:01 -0400

    product card ui

---
+++ Assets/Vincent-Scripts/ProductCardUI.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using ShopifyProductFetcher;
+
+public class ProductCardUI : MonoBehaviour
+{
+    // Public Fields:
+    public Image thumbnail;
+    public TMP_Text titleText;
+    public Button viewButton;
+
+    // Private Fields:
+    private ShopifyProductFetcher.VRItem item;
+    private ShopifyProductFetcher fetcher;
+    private int itemIndex;
+
+    public void Init(ShopifyProductFetcher.VRItem data, ShopifyProductFetcher fetcherRef)
+    {
+        item = data;
+        fetcher = fetcherRef;
+        titleText.text = item.title;
+        // Load thumbail
+        viewButton.onClick.AddListner(OnViewClicked);
+    }
+
+    private void OnViewClicked()
+    {
+        int index = fetcher.Items.IndexOf(item);
+        fetcher.InstantiateModel(index);
+    }
+}
